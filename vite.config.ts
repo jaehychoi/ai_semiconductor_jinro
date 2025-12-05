af07525e5d9006033e2021bdf18aaaca05c14eb3
jaehychoi <jhchoi8902@gmail.com>
2025-12-05 20:14:28 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // ▼▼▼ 여기가 틀려서 안 됐던 겁니다! 사진에 있는 저장소 이름으로 고쳤습니다. ▼▼▼
-  base: '/ai_semiconductor_jinro/', 
-  
+  base: '/ai_semiconductor_jinro/',
   plugins: [react()],
   resolve: {
     alias: {
